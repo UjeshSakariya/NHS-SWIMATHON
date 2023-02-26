1b914d683f2efa630086d84869e30fa34632fdea
UjeshSakariya <93031888+UjeshSakariya@users.noreply.github.com>
2023-02-26 14:58:37 +0000

    Update header.js

--- header.js
+++ header.js
@@ -11,23 +11,23 @@
           <link href="styles.css" rel="stylesheet">
           <nav class="navbar navbar-expand bg-body-tertiary" style="background-color: #e3f2fd;">
           <div class="container-fluid">
-            <a class=" main2 navbar-brand" href="index.html" target="_top">HOMEPAGE</a>
+            <a class=" new navbar-brand" href="index.html" target="_top">HOMEPAGE</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                  <a class=" main2 nav-link" href="charity.html">CHARITY</a>
+                  <a class=" new nav-link" href="charity.html">CHARITY</a>
                 </li>
                 <li class="nav-item">
-                  <a class=" main2 nav-link active" aria-current="page" href="results.html" target="_top">RESULTS</a>
+                  <a class=" new nav-link active" aria-current="page" href="results.html" target="_top">RESULTS</a>
                 </li>
-                 <li class="nav-item">
-                  <a class=" main2 nav-link active" aria-current="page"  target="_top">Click on the tags to to the left to view the other pages of the website</a>
-                </li>
+               
       </nav>
         </nav>
+        <br>
+        <p class = new > Click on the above tags to view the other webpages </p>
 
 
           `;
